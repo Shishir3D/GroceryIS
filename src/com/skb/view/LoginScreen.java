5dfea6304f19da4927fbf11cc8b102c60a38f203
shishir3d <shishirpoudel2061@gmail.com>
2024-12-26 20:09:51 +0545

    design: make login screen

--- src/com/skb/view/LoginScreen.java
+++ src/com/skb/view/LoginScreen.java
@@ -26,21 +26,65 @@ public class LoginScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        pnlLoginScreenForegroundWhiteRectangle = new javax.swing.JPanel();
+        lblLoginScreenLoginText = new javax.swing.JLabel();
+        lblLoginScreenUsername = new javax.swing.JLabel();
+        lblLoginScreenPassword = new javax.swing.JLabel();
+        textFieldLoginScreenUsername = new javax.swing.JTextField();
+        textFieldLoginScreenPassword = new javax.swing.JTextField();
+        btnLoginScreenLogin = new javax.swing.JButton();
+        lblLoginScreenForgotPassword = new javax.swing.JLabel();
+        lblLoginScreenBgImage = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1280, 720));
+        setMinimumSize(new java.awt.Dimension(1280, 720));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        pnlLoginScreenForegroundWhiteRectangle.setBackground(new java.awt.Color(255, 255, 255));
+        pnlLoginScreenForegroundWhiteRectangle.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblLoginScreenLoginText.setFont(new java.awt.Font("Liberation Sans", 0, 36)); // NOI18N
+        lblLoginScreenLoginText.setText("Login");
+        pnlLoginScreenForegroundWhiteRectangle.add(lblLoginScreenLoginText, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, -1, -1));
+
+        lblLoginScreenUsername.setForeground(new java.awt.Color(51, 51, 51));
+        lblLoginScreenUsername.setText("Username");
+        pnlLoginScreenForegroundWhiteRectangle.add(lblLoginScreenUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 140, -1, -1));
+
+        lblLoginScreenPassword.setForeground(new java.awt.Color(51, 51, 51));
+        lblLoginScreenPassword.setText("Password");
+        pnlLoginScreenForegroundWhiteRectangle.add(lblLoginScreenPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, -1, -1));
+
+        textFieldLoginScreenUsername.setText(" ");
+        textFieldLoginScreenUsername.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textFieldLoginScreenUsernameActionPerformed(evt);
+            }
+        });
+        pnlLoginScreenForegroundWhiteRectangle.add(textFieldLoginScreenUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 260, 50));
+        pnlLoginScreenForegroundWhiteRectangle.add(textFieldLoginScreenPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 250, 260, 50));
+
+        btnLoginScreenLogin.setText("LOGIN");
+        pnlLoginScreenForegroundWhiteRectangle.add(btnLoginScreenLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 350, 250, 40));
+
+        lblLoginScreenForgotPassword.setForeground(new java.awt.Color(153, 153, 153));
+        lblLoginScreenForgotPassword.setText("forgot password?");
+        pnlLoginScreenForegroundWhiteRectangle.add(lblLoginScreenForgotPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 400, -1, -1));
+
+        getContentPane().add(pnlLoginScreenForegroundWhiteRectangle, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 90, 380, 530));
+
+        lblLoginScreenBgImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/skb/resoruces/loginScreenBgImage.jpg"))); // NOI18N
+        lblLoginScreenBgImage.setText(" ");
+        getContentPane().add(lblLoginScreenBgImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1280, 720));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void textFieldLoginScreenUsernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textFieldLoginScreenUsernameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textFieldLoginScreenUsernameActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +122,14 @@ public class LoginScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLoginScreenLogin;
+    private javax.swing.JLabel lblLoginScreenBgImage;
+    private javax.swing.JLabel lblLoginScreenForgotPassword;
+    private javax.swing.JLabel lblLoginScreenLoginText;
+    private javax.swing.JLabel lblLoginScreenPassword;
+    private javax.swing.JLabel lblLoginScreenUsername;
+    private javax.swing.JPanel pnlLoginScreenForegroundWhiteRectangle;
+    private javax.swing.JTextField textFieldLoginScreenPassword;
+    private javax.swing.JTextField textFieldLoginScreenUsername;
     // End of variables declaration//GEN-END:variables
 }
